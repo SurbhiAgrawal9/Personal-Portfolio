3d733237a15c7b52c39822f2d7a8bd87b7d23691
surbhi <surbhiagrawal786@gmail.com>
2023-04-18 12:54:16 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+function BracketMatcher(str) {
+    var left = 0;
+    var right = 0;
+    for (var i = 0; i < str.length; i++) {
+        if (str[i] === "(") {
+            left++;
+        }
+        else if (str[i] === ")") {
+            right++;
+        }
+    }
+    if (right === left) {
+        return 1;
+    }
+    else {
+        return 0;
+    }
+}
